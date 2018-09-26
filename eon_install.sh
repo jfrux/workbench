b2e7fa950f43b2639907ae436ea03a14737b5ce7
Joshua F. Rountree <joshua.rountree@scripps.com>
2018-09-26 11:12:32 -0400

    Fixes output of zip file

--- eon_install.sh
+++ eon_install.sh
@@ -1,5 +1,5 @@
 cd /data
 rm -rf ./workbench/
 rm -f workbench.zip
-curl -fsSL https://github.com/openpilot-community/workbench/blob/master/workbench.zip?raw=true
+curl -fsSL https://github.com/openpilot-community/workbench/blob/master/workbench.zip?raw=true -o ./workbench.zip
 unzip ./workbench.zip
