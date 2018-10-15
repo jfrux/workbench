b6beadb4866935696413a6b15c2fd4221f7ee588
Joshua F. Rountree <joshua.rountree@scripps.com>
2018-10-14 23:58:19 -0400

    Fixes to websockets

--- workbench_api/workbenchd.py
+++ workbench_api/workbenchd.py
@@ -4,6 +4,7 @@ import sys
 import fingerprint
 import monitor
 import server
+import ws_server
 
 # def start_fingerprint():
 #   print '[started] Workbench Fingerprinter'
@@ -20,11 +21,18 @@ def start_server():
   server.rest_server()
   print '[stopped] Workbench Web Server'
 
+def start_websockets():
+  print '[started] Workbench WebSockets Server'
+  ws_server.main()
+  print '[stopped] Workbench WebSockets Server'
+
 if __name__ == '__main__':
     p1 = Process(target = start_monitor)
     p1.start()
     p2 = Process(target = start_server)
     p2.start()
+    p3 = Process(target = start_websockets)
+    p3.start()
     # DEPRECATED, NOW IS IN MONITOR
     # p3 = Process(target = start_fingerprint)
     # p3.start()
--- workbench_api/ws_server.py
+++ workbench_api/ws_server.py
@@ -5,11 +5,9 @@ def new_client(client, server):
 	print("New client connected and was given id %d" % client['id'])
 	server.send_message_to_all("Hey all, a new client has joined us")
 
-
 # Called for every client disconnecting
 def client_left(client, server):
 	print("Client(%d) disconnected" % client['id'])
-
 
 # Called when a client sends a message
 def message_received(client, server, message):
@@ -17,10 +15,13 @@ def message_received(client, server, message):
 		message = message[:200]+'..'
 	print("Client(%d) said: %s" % (client['id'], message))
 
+def main():
+  PORT=4000
+  server = WebsocketServer(PORT)
+  server.set_fn_new_client(new_client)
+  server.set_fn_client_left(client_left)
+  server.set_fn_message_received(message_received)
+  server.run_forever()
 
-PORT=4000
-server = WebsocketServer(PORT)
-server.set_fn_new_client(new_client)
-server.set_fn_client_left(client_left)
-server.set_fn_message_received(message_received)
-server.run_forever()
+if __name__ == '__main__':
+  main()
